4e3c4f4e2d2a7493d34a8399faa7e036e369a3aa
DivakarD-QA <ddiva555kar@gmail.com>
2024-04-12 22:26:25 +0530

    New file from branch3

--- Test7NewBranch.java
+++ Test7NewBranch.java
@@ -1,6 +1,7 @@
 public class Test7NewBranch {
    public static void main(String[] args) {
     System.out.println(" Created a new branch and add files");
+    System.out.println(" Changing the branch2 and update");
    }
     
 }
---
+++ Test8Branch3.java
@@ -0,0 +1,6 @@
+public class Test8Branch3 {
+    public static void main(String[] args) {
+        System.out.println(" Checking branch 3 merge");
+    }
+    
+}
