fe5f1acbd5f8f0dc72df17df9ad88aed0cf3bc8d
DivakarD-QA <ddiva555kar@gmail.com>
2024-04-12 20:03:07 +0530

    Change

--- Test1.java
+++ Test1.java
@@ -1,6 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello World I am back");
+        System.out.println(" I am Iron Man");
     }
     
 }
---
+++ Test2.java
@@ -0,0 +1,6 @@
+public class Test2 {
+    public static void main(String[] args) {
+        System.err.println(" VS cose is having some issue");
+    }
+    
+}
