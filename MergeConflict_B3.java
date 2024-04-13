eb9fc04ef2840054579f35a94631140467ac0f90
DivakarD-QA <ddiva555kar@gmail.com>
2024-04-13 11:09:22 +0530

    Edit as molnier in B4

--- MergeConflict_B3.java
+++ MergeConflict_B3.java
@@ -1,5 +1,5 @@
 public interface MergeConflict_B3 {
     public static void main(String[] args) {
-        System.out.println(" I am --> The GOD of Thunder - THOR Hammer");
+        System.out.println(" I am --> The GOD of Thunder - THOR Molnier");
     }
 }
